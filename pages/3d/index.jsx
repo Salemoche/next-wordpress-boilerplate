9da7a74cb3e0144fd06a9f8a25b65d93414c2bd2
Gabriel Bach <gabriel@inter-action.design>
2021-11-05 11:22:05 +0100

    add react fiber example

--- pages/3d/index.jsx
+++ pages/3d/index.jsx
@@ -1,5 +1,6 @@
 // Base
 import { useEffect } from 'react';
+import React, { useRef, useState } from 'react'
 
 // Data
 
@@ -9,28 +10,33 @@ import { useEffect } from 'react';
 import { motion } from 'framer-motion';
 import LayoutComponent from '../../components/layout/layout.component';
 
-// function Box(props) {
-//     // This reference gives us direct access to the THREE.Mesh object
-//     const ref = useRef()
-//     // Hold state for hovered and clicked events
-//     const [hovered, hover] = useState(false)
-//     const [clicked, click] = useState(false)
-//     // Subscribe this component to the render-loop, rotate the mesh every frame
-//     useFrame((state, delta) => (ref.current.rotation.x += 0.01))
-//     // Return the view, these are regular Threejs elements expressed in JSX
-//     return (
-//         <mesh
-//             {...props}
-//             ref={ref}
-//             scale={clicked ? 1.5 : 1}
-//             onClick={(event) => click(!clicked)}
-//             onPointerOver={(event) => hover(true)}
-//             onPointerOut={(event) => hover(false)}>
-//             <boxGeometry args={[1, 1, 1]} />
-//             <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
-//       </mesh>
-//     )
-// }
+// 3D
+import { Canvas, useFrame } from '@react-three/fiber'
+import { ContentStyles } from '../../../1_wordpress/styles/global-components.styles';
+import { FiberStyles } from './3d.styles';
+
+function Box(props) {
+    // This reference gives us direct access to the THREE.Mesh object
+    const ref = useRef()
+    // Hold state for hovered and clicked events
+    const [hovered, hover] = useState(false)
+    const [clicked, click] = useState(false)
+    // Subscribe this component to the render-loop, rotate the mesh every frame
+    useFrame((state, delta) => (ref.current.rotation.x += 0.01))
+    // Return the view, these are regular Threejs elements expressed in JSX
+    return (
+        <mesh
+            {...props}
+            ref={ref}
+            scale={clicked ? 1.5 : 1}
+            onClick={(event) => click(!clicked)}
+            onPointerOver={(event) => hover(true)}
+            onPointerOut={(event) => hover(false)}>
+            <boxGeometry args={[1, 1, 1]} />
+            <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+      </mesh>
+    )
+}
 
 export default function Fiber ({ }) {
 
@@ -42,8 +48,15 @@ export default function Fiber ({ }) {
             exit={{ opacity: 0 }}
             transition={{ duration: .6 }}
         >
-            <LayoutComponent>
-                <h1>Hello, world</h1> 
+            <LayoutComponent hasNavigation={false} hasFooter={false}>
+                <FiberStyles>
+                    <Canvas>
+                        <ambientLight />
+                        <pointLight position={[10, 10, 10]} />
+                        <Box position={[-1.2, 0, 0]} />
+                        <Box position={[1.2, 0, 0]} />
+                    </Canvas>
+                </FiberStyles>
             </LayoutComponent>
         </motion.div>
     )
