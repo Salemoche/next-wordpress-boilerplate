4de0070d76f5fb6ddfeaedaa49a5f38658ddd112
Gabriel Bach <gabriel@inter-action.design>
2022-01-17 15:05:49 +0100

    recenter footer

--- components/footer/footer.styles.jsx
+++ components/footer/footer.styles.jsx
@@ -1,7 +1,12 @@
 import styled from 'styled-components'
-import { ContentStyles } from '../../styles/global-components.styles';
+import theme from '../../../1_wordpress/styles/theme'
+// import { ContentStyles } from '../../styles/global-components.styles';
 
-export const FooterStyles = styled(ContentStyles)`
+export const FooterStyles = styled('div')`
+    width: ${ theme.contentWidthL }px;
+    max-width: 100%;
+    margin: 0 auto;
+    padding: ${ theme.spaceM }px;
     display: flex;
     justify-content: center;
 
